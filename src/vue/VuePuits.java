f8fd17f024df1b858e568f5d56381e31a74063fa
Othmane Mansouri <othmane.mansouri@outlook.com>
2023-03-26 16:47:38 +0200

    VuePuits + tests

--- src/vue/VuePuits.java
+++ src/vue/VuePuits.java
@@ -1,4 +1,54 @@
 package fr.eseo.e3.poo.projet.blox.vue;
 
-public class VuePuits {
+import fr.eseo.e3.poo.projet.blox.modele.Puits;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class VuePuits extends JPanel {
+    public static final int TAILLE_PAR_DEFAUT = 10;
+    private Puits puits;
+    private int taille;
+
+    public VuePuits(Puits puits) {
+        this.puits = puits;
+        this.taille = TAILLE_PAR_DEFAUT;
+        setPreferredSize(new Dimension(400, 400));
+        setBackground(Color.WHITE);
+    }
+
+    public VuePuits(Puits puits, int taille) {
+        this.puits = puits;
+        this.taille = taille;
+        setPreferredSize(new Dimension(400, 400));
+        setBackground(Color.WHITE);
+    }
+
+    public Puits getPuits() {
+        return puits;
+    }
+
+    public int getTaille() {
+        return taille;
+    }
+
+    public void setPuits(Puits puits) {
+        this.puits = puits;
+    }
+
+    public void setTaille(int taille) {
+        this.taille = taille;
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2D = (Graphics2D)g.create();
+        g2D.setColor(Color.LIGHT_GRAY);
+        for (int x = 0; x < getWidth(); x += taille) {
+            for (int y = 0; y < getHeight(); y += taille) {
+                g2D.fillRect(x, y, taille, taille);
+            }
+        }
+        g2D.dispose();
+    }
 }
--- test/vue/VuePuitsAffichageTest.java
+++ test/vue/VuePuitsAffichageTest.java
@@ -1,4 +1,65 @@
 package fr.eseo.e3.poo.projet.blox.vue;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import fr.eseo.e3.poo.projet.blox.modele.Puits;
+
 public class VuePuitsAffichageTest {
+
+    public VuePuitsAffichageTest() {
+        testConstructeurPuits();
+        testConstructeurPuitsTaille();
+    }
+
+    private void testConstructeurPuits() {
+                // Créer une instance de Puits
+                Puits puits = new Puits();
+
+                // Créer une instance de VuePuits avec le puits
+                VuePuits vuePuits = new VuePuits(puits);
+
+                // Créer une JFrame avec la vuePuits
+                JFrame frame = new JFrame("Puits");
+                frame.getContentPane().add(vuePuits);
+
+                // Définir la taille de la fenêtre en fonction de la vuePuits
+                frame.pack();
+
+                // Centrer la fenêtre sur l'écran
+                frame.setLocationRelativeTo(null);
+
+                // Fermer l'application quand la fenêtre est fermée
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void testConstructeurPuitsTaille() {
+                // Créer une instance de Puits
+                Puits puits = new Puits();
+
+                // Créer une instance de VuePuits avec le puits et une taille de 100
+                VuePuits vuePuits = new VuePuits(puits, 100);
+
+                // Créer une JFrame avec la vuePuits
+                JFrame frame = new JFrame("Puits et taille");
+                frame.getContentPane().add(vuePuits);
+
+                // Définir la taille de la fenêtre en fonction de la vuePuits
+                frame.pack();
+
+                // Centrer la fenêtre sur l'écran
+                frame.setLocationRelativeTo(null);
+
+                // Fermer l'application quand la fenêtre est fermée
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main (String [] args) {
+        SwingUtilities.invokeLater(new Runnable () {
+            @Override
+            public void run() {
+                new VuePuitsAffichageTest();
+            }
+        });
+    }
 }
