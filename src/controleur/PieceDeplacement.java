1b795f9f2fe8d9d05ce88de1c98eedb3fb9e2986
Othmane Mansouri <othmane.mansouri@outlook.com>
2023-03-29 01:07:35 +0200

    PieceDeplacement...

--- src/controleur/PieceDeplacement.java
+++ src/controleur/PieceDeplacement.java
@@ -1,4 +1,20 @@
 package fr.eseo.e3.poo.projet.blox.controleur;
+import fr.eseo.e3.poo.projet.blox.vue.VuePuits;
 
-public class PieceDeplacement {
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class PieceDeplacement extends KeyAdapter {
+
+    private VuePuits vuePuits;
+    public PieceDeplacement(VuePuits vuePuits) {
+        this.vuePuits = vuePuits;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (vuePuits.getPuits().getPieceActuelle() != null) {
+
+        }
+    }
 }
